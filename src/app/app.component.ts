ed39643ba90d46c91a511e75040f8b5cf0d837d7
SHEFFcode <sheff@sheffmachine.com>
2016-07-26 22:48:09 -0700

    first commit of the recipe book A2 project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'js-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'app works!';
+
 }
