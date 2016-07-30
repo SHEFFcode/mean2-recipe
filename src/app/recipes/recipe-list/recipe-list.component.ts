a6944dc02b071a848fa42ede15ff5b3a3b59b3d8
SHEFFcode <sheff@sheffmachine.com>
2016-07-29 19:18:29 -0700

    impelement recipe item class and modify the recipe list and recipe component to include the correct directives

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe";
+import {RecipeItemComponent} from "./recipe-item.component";
 
 @Component({
   moduleId: module.id,
   selector: 'js-recipe-list',
-  templateUrl: 'recipe-list.component.html'
+  templateUrl: 'recipe-list.component.html',
+  directives: [RecipeItemComponent]
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
+  recipe = new Recipe('Dummy', 'Dummy', 'http://guides.global/images/guides/global/dummy_web_page.jpg');
 
   constructor() { }
 
