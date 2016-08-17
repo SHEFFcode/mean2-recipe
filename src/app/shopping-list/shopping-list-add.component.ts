773ef5b768850784f6d6ba4f714a9d58cecc4f92
SHEFFcode <sheff@sheffmachine.com>
2016-08-16 21:33:43 -0700

    add shopping list add / edit component

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, OnChanges} from '@angular/core';
 import {Ingredient} from "../shared/ingredient";
 import {ShoppingListService} from "./shopping-list.service";
 
@@ -7,13 +7,25 @@ import {ShoppingListService} from "./shopping-list.service";
   selector: 'js-shopping-list-add',
   templateUrl: 'shopping-list-add.component.html'
 })
-export class ShoppingListAddComponent implements OnInit {
+export class ShoppingListAddComponent implements OnInit, OnChanges {
   isAdd = true;
-  item: Ingredient;
+  @Input() item: Ingredient;
 
   constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes){
+    if (changes.item.currentValue === null) {
+      this.isAdd = true;
+      this.item = {
+       name: null,
+        amount: null
+      };
+    } else {
+      this.isAdd = false;
+    }
   }
 
   onSubmit(ingredient: Ingredient) {
