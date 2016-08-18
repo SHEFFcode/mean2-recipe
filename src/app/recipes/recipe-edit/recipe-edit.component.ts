74374bc257a2143085072fdbef00041d4a718dfe
SHEFFcode <sheff@sheffmachine.com>
2016-08-17 18:42:08 -0700

    set up the subsription to route params

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {RecipeService} from "../recipe.service";
+import {Subscription} from "rxjs";
 
 @Component({
   moduleId: module.id,
@@ -7,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class RecipeEditComponent implements OnInit {
+  private recipeIndex: number;
+  private subscription: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService) { }
 
   ngOnInit() {
+    let isNew: boolean = true;
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+      if (params.hasOwnProperty('id')) {
+        isNew = false;
+        this.recipeIndex = +params['id'];
+      } else {
+        isNew = true;
+      }
+    });
   }
 
 }
